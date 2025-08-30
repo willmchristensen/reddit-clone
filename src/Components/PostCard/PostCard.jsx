3ec56f6345cd74b8c5759e9c55d3dd83c8654ce3
willmchristensen <willmchristensen@gmail.com>
2025-08-30 15:14:32 -0700

    postcard

--- src/Components/PostCard/PostCard.jsx
+++ src/Components/PostCard/PostCard.jsx
@@ -1,24 +1,26 @@
 import '../PostCard.css';
 function PostCard({}){
     return (
-        <div className="post-card-container">
-            <div className="votes">
-                <div className="votes-wrapper">
-                    <i className="fas fa-arrow-up"></i>
-                    <span>123</span>
-                    <i className="fas fa-arrow-down"></i>
+        <div className="post-card-wrapper">
+            <div className="post-card-container">
+                <div className="votes">
+                    <div className="votes-wrapper">
+                        <i className="fas fa-arrow-up"></i>
+                        <span>123</span>
+                        <i className="fas fa-arrow-down"></i>
+                    </div>
                 </div>
-            </div>
-            <div className="title">
-                <h1>title</h1>
-            </div>
-            <div className="image">
-                <h2>image</h2>
-            </div>
-            <div className="post-data">
-                <span>posted by</span>
-                <span>7 hours ago</span>
-                <span>700</span>
+                <div className="title">
+                    <h1>title</h1>
+                </div>
+                <div className="image">
+                    <h2>image</h2>
+                </div>
+                <div className="post-data">
+                    <span>posted by</span>
+                    <span>7 hours ago</span>
+                    <span>700</span>
+                </div>
             </div>
         </div>
     )
